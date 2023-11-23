e2c98cfd675b7b8e0c9550f9e0d8ccb720a17f5f
maddie <lee.maddie@gmail.com>
2023-11-23 10:44:13 -0500

    Add completed seed

--- db/seeds/03_to_dos.sql
+++ db/seeds/03_to_dos.sql
@@ -11,3 +11,4 @@
 
 INSERT INTO to_dos (name, category_id, user_id, created_at, due_date, completed_at, is_complete, priority, tagged_user) VALUES ('Barbie', 1, 2, CURRENT_TIMESTAMP, '2023-12-31', NULL, FALSE, NULL, 3);
 INSERT INTO to_dos (name, category_id, user_id, created_at, due_date, completed_at, is_complete, priority, tagged_user) VALUES ('Imanishi Japanese Kitchen', 2, 1, CURRENT_TIMESTAMP, NULL, NULL, FALSE, 2, NULL);
+INSERT INTO to_dos (name, category_id, user_id, created_at, due_date, completed_at, is_complete, priority, tagged_user) VALUES ('Scott Pilgrim Takes Off', 1, 2, '2022-01-01 10:00:00', NULL, '2023-11-22', TRUE, NULL, NULL);
