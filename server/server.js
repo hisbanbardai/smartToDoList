e53e3c9f7410ce0bcae378b0fac6c916869e736c
Hisban Shiraz <hisbanbardai@gmail.com>
2023-11-23 16:12:39 -0700

    Setup login router object

--- server/server.js
+++ server/server.js
@@ -31,6 +31,7 @@ app.use(express.static('public'));
 const userApiRoutes = require('../routes/users-api');
 const widgetApiRoutes = require('../routes/widgets-api');
 const usersRoutes = require('../routes/users');
+const loginRoutes = requrie('../routes/login');
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
