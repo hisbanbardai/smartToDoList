e4aa49dbeec7893ec3868ae532cce689b4d3cca0
maddie <lee.maddie@gmail.com>
2023-11-25 16:51:08 -0500

    Rewrite getToDos to get todos by category

--- db/queries/to_dos.js
+++ db/queries/to_dos.js
@@ -1,10 +1,20 @@
 const db = require('../connection');
 
-const getToDos = () => {
-  return db.query('SELECT * FROM to_dos;')
+// const getToDos = () => {
+//   return db.query('SELECT * FROM to_dos;')
+//     .then(data => {
+//       return data.rows;
+//     });
+// };
+
+const getToDosByCategory = (category) => {
+  return db.query(`
+  SELECT *
+  FROM to_dos
+  WHERE category = ${category};`)
     .then(data => {
       return data.rows;
     });
 };
 
-module.exports = { getToDos };
+module.exports = { getToDosByCategory };
