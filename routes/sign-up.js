516ea1dc92566aefb9eb0075c7d898eb6efd32c2
maddie <lee.maddie@gmail.com>
2023-11-26 17:52:58 -0500

    Implement post route with addUser

--- routes/sign-up.js
+++ routes/sign-up.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const cookieSession = require("cookie-session");
+const { addUser } = require("../db/queries/users");
 
 router.use(
   cookieSession({
@@ -13,17 +14,30 @@ router.get("/", (req, res) => {
   const userId = req.session.user_id;
 
   const templateVars = {
-    user: userId 
+    user: userId
   };
 
   if(userId) {
     return res.redirect("/");
   }
 
-  res.render("sign-up", templateVars);  
+  res.render("sign-up", templateVars);
 });
 
 router.post("/", (req, res) => {
+  const { email, password } = req.body;
+  const user = {
+    email,
+    password
+  };
+
+  addUser(user)
+  .then((id) => {
+    console.log(id);
+    req.session.user_id = id;
+    res.redirect("/");
+  })
+  .catch((err) => console.log(err.message));
 
 });
 
