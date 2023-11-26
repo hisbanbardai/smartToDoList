e7b64cf50df0cae572c463dfa87c382482ad31d1
maddie <lee.maddie@gmail.com>
2023-11-26 09:23:48 -0500

    Lint code and clean up errors

--- db/queries/categories.js
+++ db/queries/categories.js
@@ -8,12 +8,12 @@ const getCategoryByName = (name) => {
   const queryParams = [ `%${name}%` ];
 
   return db.query(queryString, queryParams)
-  .then((data) => {
-    return data.rows;
-  })
-  .catch((err) => {
-    console.log(err.message);
-  });
+    .then((data) => {
+      return data.rows;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
 
-module.exports = { getCategoryById };
+module.exports = { getCategoryByName };
