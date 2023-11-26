ba4525f4436b3c3cdaa65d53c5454f58a237756b
maddie <lee.maddie@gmail.com>
2023-11-26 08:41:30 -0500

    Write getCategoryById

--- db/queries/categories.js
+++ db/queries/categories.js
@@ -1,1 +1,16 @@
 const db = require('../connection');
+
+const getCategoryById = (id) => {
+  const queryString = `SELECT name
+  FROM categories
+  WHERE id = $1`;
+  const queryParams = [id];
+
+  return db.query(queryString, queryParams)
+  .then((data) => {
+    return data.rows;
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+};
