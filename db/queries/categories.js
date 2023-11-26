8a27f8280125fda02ddf8c62decd8bee31cecc55
maddie <lee.maddie@gmail.com>
2023-11-26 09:17:43 -0500

    Create getCategoryByName function and export

--- db/queries/categories.js
+++ db/queries/categories.js
@@ -1,1 +1,19 @@
 const db = require('../connection');
+
+// Return ID of category based on name
+const getCategoryByName = (name) => {
+  const queryString = `SELECT id
+  FROM categories
+  WHERE name LIKE $1`;
+  const queryParams = [ `%${name}%` ];
+
+  return db.query(queryString, queryParams)
+  .then((data) => {
+    return data.rows;
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+};
+
+module.exports = { getCategoryById };
