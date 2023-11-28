5e5f88ab4a2966772d47288c81d1544e8126f3c9
maddie <lee.maddie@gmail.com>
2023-11-27 21:50:02 -0500

    Fix import of getUserById

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 8080;
 const app = express();
 
 const cookieSession = require('cookie-session');
-const getUserById = require('../db/queries/users');
+const { getUserById } = require('../db/queries/users');
 
 app.set('view engine', 'ejs');
 
