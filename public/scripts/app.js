1fe8390698ea1dedeead842d5bbd5d294916e24e
Hisban Shiraz <hisbanbardai@gmail.com>
2023-11-27 23:11:35 -0700

    Render todo items in the right column based on both user and category

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,35 +1,41 @@
 // Client facing scripts here
 
+$(document).ready(function () {
+  const renderTodos = function (response) {
+    const toDoCategory = {
+      1: "watch-todo-list",
+      2: "eat-todo-list",
+      3: "read-todo-list",
+      4: "buy-todo-list",
+    };
 
-$(document).ready(function() {
+    let $todosList;
 
-  const renderTodos = function(response) {
-    console.log(response);
-    const $todosList = $('#eat-todo-list');
+    //no data should be displayed initially
+    for (const category in toDoCategory) {
+      $todosList = $(`#${toDoCategory[category]}`);
       $todosList.empty();
+    }
 
+    for (const todo of response.toDos) {
+      $todosList = $(`#${toDoCategory[todo.category_id]}`);
+      console.log($todosList);
+      const element = createToDoElement(todo);
+      $todosList.append(element);
+    }
 
-      for(const todo of response.toDos) {
-        
-        const element = createToDoElement(todo);
-        element.data('todo', todo)
-        $todosList.prepend(element);
+    console.log(response);
+  };
 
-      }
-    
-  }
-  const createToDoElement = function(todo) {
-    const element = 
-    `
+  const createToDoElement = function (todo) {
+    const element = `
     <div class='todo'>
     <h3>${todo.name}</h3>
     <button class='delete-todo'>Delete</button>
     </div>
     `;
-
-    return $(element).data('todo', todo);
-  }
-
+    return $(element).data("todo", todo);
+  };
 
   // Define an escape function to safely escape HTML content
   // const escape = function(str) {
@@ -38,35 +44,31 @@ $(document).ready(function() {
   //   return div.innerHTML;
   // };
 
-  //Function to delete a todo 
-  $('.todo-main-container').on('click', '.delete-todo', function(event) {
-
-    console.log('Deleting');
+  //Function to delete a todo
+  $(".todo-main-container").on("click", ".delete-todo", function (event) {
+    console.log("Deleting");
     const todoElement = $(this).parent();
     console.log(todoElement);
-    const todo = todoElement.data('todo');
+    const todo = todoElement.data("todo");
     console.log(todo);
-    
+
     $.ajax({
       url: `/api/todo/${todo.id}`,
-      type: 'DELETE',
+      type: "DELETE",
     })
-    .then(() => {
-      console.log('delete todo');
-      todoElement.remove();
-    })
-    .catch(error => console.log("Error: ", error)); 
-
-
+      .then(() => {
+        console.log("delete todo");
+        todoElement.remove();
+      })
+      .catch((error) => console.log("Error: ", error));
   });
 
   // Function to submit the form
-  $('#myForm').on('submit', function(event) {
-    
+  $("#myForm").on("submit", function (event) {
     event.preventDefault();
-    
+
     // Serialize the form data
-    const todo = $('#myForm').serialize();
+    const todo = $("#myForm").serialize();
     console.log($(this).serialize());
 
     $.post("/api/todo", todo)
@@ -74,26 +76,21 @@ $(document).ready(function() {
         console.log(data);
         $loadTodos();
       })
-      .catch(error => console.log(error));
-    
-    $("#todo-text").val(" "); // clear the text after submitting the form
+      .catch((error) => console.log(error));
 
+    $("#todo-text").val(" "); // clear the text after submitting the form
   });
 
   // Function to load todos
-  const $loadTodos = function() {
-    console.log('test');      
+  const $loadTodos = function () {
+    console.log("test");
     $.ajax({
-      url: '/api/todo',
-      method: 'GET',
+      url: "/api/todo",
+      method: "GET",
     })
-    .then(renderTodos)
-    .catch(error => console.log("Error: ", error)); 
-  }
+      .then((renderTodos))
+      .catch((error) => console.log("Error: ", error.responseText));
+  };
 
   $loadTodos();
-  
 });
-
-
-
