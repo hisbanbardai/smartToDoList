a9fe262007d244fbf71106c30a9debf2b35496fa
maddie <lee.maddie@gmail.com>
2023-11-29 14:54:12 -0500

    Render user data on page

--- routes/users.js
+++ routes/users.js
@@ -7,13 +7,26 @@
 
 const express = require('express');
 const router  = express.Router();
+const cookieSession = require('cookie-session');
+const { getUserById } = require('../db/queries/users');
+
+router.use(
+  cookieSession({
+    name: "session",
+    keys: ["key1", "key2"],
+  })
+);
 
 router.get('/', (req, res) => {
   res.render('users');
 });
 
 router.get('/:id', (req, res) => {
-  res.render('profile');
+  getUserById(req.session.user_id)
+  .then((user) => {
+    const templateVars = { user };
+    res.render('profile', templateVars);
+  })
 });
 
 module.exports = router;
